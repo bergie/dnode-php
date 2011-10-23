4c9c0028aab1faff8c4124e051aab36df7cf5b5d
Henri Bergius <henri.bergius@iki.fi>
2011-10-23 14:56:46 +0300

    A bit of documentation

--- examples/simple/server.php
+++ examples/simple/server.php
@@ -2,10 +2,13 @@
 // Include standard autoloader
 require(__DIR__.'/../autoloader.php');
 
+// This is the class we're exposing to DNode
 class Zinger
 {
+    // Public methods are made available to the network
     public function zing($n, $cb)
     {
+        // Dnode is async, so we return via callback
         $cb($n * 100);
     }
 }
