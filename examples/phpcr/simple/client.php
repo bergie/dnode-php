31c23853ceb8f0a5ab9878a00dabbe5ce58dab72
piotras <piotrek.pokora@gmail.com>
2011-11-14 09:16:41 +0100

    getProperty

--- examples/phpcr/simple/client.php
+++ examples/phpcr/simple/client.php
@@ -5,8 +5,10 @@ require(__DIR__.'/../../vendor/.composer/autoload.php');
 // Connect to DNode server running in port 7070 and call Zing with argument 33
 $dnode = new DNode\DNode();
 $dnode->connect(7070, function($remote, $connection) {
-    $remote->execute("default", "getPropertyValue", null, function($n) use ($connection) {
-        echo "n = {$n}\n";
+    $remote->getPropertyValue("default", "/jcr:primaryType", function($val, $exc, $error) use ($connection) {
+        echo $val;
+        if ($exc != null)
+            echo "Exception {$exc} thrown with message {$error} \n";
         $connection->end();
     });
 });
