e8f4641d3a56b051ff8f3ebf1de7498761484e1b
Igor Wiedler <igor@wiedler.ch>
2012-11-01 22:36:36 +0100

    Add initial functional test case

---
+++ tests/DNode/FunctionalTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace DNode;
+use React\EventLoop\StreamSelectLoop;
+
+class FunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function transformerShouldRespondCorrectly()
+    {
+        $captured = null;
+
+        $loop = new StreamSelectLoop();
+
+        $server = new DNode($loop, new Transformer());
+        $socket = $server->listen(5004);
+
+        $client = new DNode($loop);
+        $client->connect(5004, function ($remote, $conn) use (&$captured, $socket) {
+            $remote->transform('fou', function ($transformed) use ($conn, &$captured, $socket) {
+                $captured = $transformed;
+                $conn->end();
+                $socket->shutdown();
+            });
+        });
+
+        $loop->run();
+
+        $this->assertSame('FOO', $captured);
+    }
+}
+
+class Transformer
+{
+    public function transform($input, $callback)
+    {
+        $callback(strtoupper(preg_replace('/[aeiou]{2,}/', 'oo', $input)));
+    }
+}
